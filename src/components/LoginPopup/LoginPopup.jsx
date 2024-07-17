becd3228a7efe0b2ac0d26f453263856ccc21e43
Shivang74 <98741995+Shivang74@users.noreply.github.com>
2024-07-17 14:29:33 +0530

    Update LoginPopup.jsx

--- src/components/LoginPopup/LoginPopup.jsx
+++ src/components/LoginPopup/LoginPopup.jsx
@@ -3,6 +3,9 @@ import './LoginPopup.css'
 import { assets } from '../../assets/assets'
 const LoginPopup = ({setShowLogin}) => {
     const [currState,setCurrState] = useState("Login")
+    function showAlert() {
+    alert('We are working on this, please wait for some time.');
+}
   return (
     <div className='login-popup'>
         <form  className="login-popup-container">
@@ -15,7 +18,7 @@ const LoginPopup = ({setShowLogin}) => {
                 <input type="email" placeholder='Your E-mail' required />
                 <input type="password" placeholder='Password' required />
             </div>
-            <button onclick="(() => alert('Sorry! This functionality is in maintanence, please wait for a while'))()"> {currState==="Sign Up"?"Create Account":"Login"}</button>
+            <button onclick="showAlert()"> {currState==="Sign Up"?"Create Account":"Login"}</button>
             <div className="login-popup-condition">
                 <input type="checkbox" required />
                 <p>By proceeding, I agree to terms and privacy policy</p>
